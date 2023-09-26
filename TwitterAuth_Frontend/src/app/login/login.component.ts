f25891c838fad98da03d05478d3eca3cb9810c3e
“karandepale” <karandepale111@gmail.com>
2023-09-26 16:07:58 +0530

    [Karan] Add : Added Login Component

---
+++ TwitterAuth_Frontend/src/app/login/login.component.ts
@@ -0,0 +1,46 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  private requestToken: Partial<RequestToken> = {};
+  disableButton = false;
+  isLoading = false;
+
+  constructor(private twitterService: TwitterAuthService, private route: ActivatedRoute, private router: Router) 
+  { 
+
+    this.route.queryParamMap.subscribe(params => {
+      const oauth_token = this.route.snapshot.queryParamMap.get('oauth_token');
+      const oauth_verifier = this.route.snapshot.queryParamMap.get("oauth_verifier");
+      if (oauth_token && oauth_verifier) {
+        this.disableButton = true;
+        this.isLoading = true;
+        this.twitterService.getAccessToken(oauth_token, oauth_verifier).subscribe(null, error => console.log(error)
+        ,() => {
+          this.router.navigate(['/Home']);
+        });
+      }
+    });
+    
+    }
+  ngOnInit() {
+
+  }
+
+launchTwitterLogin() {
+this.isLoading = true;
+this.disableButton = true;
+this.twitterService.getRequestToken()
+ .subscribe(response => this.requestToken = response, 
+   error => console.log(error), 
+   () => {
+    console.log(this.requestToken);
+   location.href = "https://api.twitter.com/oauth/authenticate?" + this.requestToken.oauth_token;
+   }
+ );
+}
