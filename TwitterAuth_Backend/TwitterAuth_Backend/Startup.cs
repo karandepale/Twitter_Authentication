90fae82658d75a9cc1b8f116433b562212bf4c94
“karandepale” <karandepale111@gmail.com>
2023-09-26 15:19:36 +0530

    [Karan] Add : Added Swagger and Cors

--- TwitterAuth_Backend/TwitterAuth_Backend/Startup.cs
+++ TwitterAuth_Backend/TwitterAuth_Backend/Startup.cs
@@ -6,10 +6,14 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TwitterAuth_Backend.Data;
+using TwitterAuth_Backend.Model;
+
 
 namespace TwitterAuth_Backend
 {
@@ -26,11 +30,66 @@ namespace TwitterAuth_Backend
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            //SWAGGER:-
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Twitter SignIn",
+                    Description = "Sign In API's",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Karan Depale",
+                        Email = "karandepale111@gmail.com",
+                    },
+                });
+            });
+
+
+            //CORS SERVICE CONFIGURATION:-
+            services.AddCors(data =>
+            {
+                data.AddPolicy(
+                    name: "AllowOrigin",
+                    builder =>
+                    {
+                        builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+                    });
+            });
+
+            //TWITTER CONFIGURATION:-
+            services.Configure<TwitterSettings>(Configuration.GetSection("TwitterSettings"));
+            services.AddHttpClient("twitter");
+            services.AddScoped<ITwitterAuthRepository, TwitterAuthRepository>();
+
+
+
         }
+
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+
+
+            //SWAGGER MIDDLEWARE:-
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Twitter SignIn V1");
+            });
+
+
+
+
+            //CORS MIDDLEWARE:-
+            app.UseCors("AllowOrigin");
+
+
+
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
