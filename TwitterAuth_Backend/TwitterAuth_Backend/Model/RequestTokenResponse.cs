809959f4b128063469aab8ae7dcf45ba397b96e2
“karandepale” <karandepale111@gmail.com>
2023-09-26 14:37:05 +0530

    [Karan] Add : RequestTokenResponse Model Added for getting Token Request

---
+++ TwitterAuth_Backend/TwitterAuth_Backend/Model/RequestTokenResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace TwitterAuth_Backend.Model
+{
+    public class RequestTokenResponse
+    {
+        public string oauth_token { get; set; }
+        public string oauth_token_secret { get; set; }
+        public string oauth_callback_confirmed { get; set; }
+    }
+}
